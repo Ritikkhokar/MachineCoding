d6ffc6a3c48c05fc8fbc070e27c9677a8019de6a
Ritik Khokar <khokharritik1999@gmail.com>
2026-05-14 20:55:30 -0500

    connect four gmae code started

--- BuildConnectFourGame/ConnectFourBoard.java
+++ BuildConnectFourGame/ConnectFourBoard.java
@@ -5,5 +5,21 @@ public class ConnectFourBoard {
 
     public ConnectFourBoard() {
         board = new char[6][7];
+        for(int i=0;i<6;i++){
+            for(int j=0;j<7;j++){
+                board[i][j] = '-';
+            }
+        }
     }
+
+    public boolean isValidMove(int column) {
+        if(column<1 || column>7)return false;
+        if(board[0][column-1] != '-') return false;
+        return true;
+    }
+
+    
+
+
+        
 }
